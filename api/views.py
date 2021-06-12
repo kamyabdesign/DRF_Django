7930ef9ad259c092e16e7ae1885054cef1608415
kamyabdesign <faizi.kamyabdesign@gmail.com>
2021-06-12 16:50:41 +0430

    articleDetail

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 # from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from blog.models import Article
 from .serializers import ArticleSerializer
 
 
 class ArticleList(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
